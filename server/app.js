00c308e4a7df923180322b8c216be4b06adb0250
lowie1803 <dung.totuan01@gmail.com>
2023-12-15 21:28:51 +0700

    Unfix mongodb uri to address PR comment

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const profileRouter = require('./routes/profile');
 const app = express();
 
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb://127.0.0.1/LC_db"
+const mongoDB = process.env.MONGODB_URI
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -27,7 +27,6 @@ async function main() {
 app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
 
-// app.use("/api", indexRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,25 +39,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/api/index',authFilter)
-
 app.use('/api/index', indexRouter);
 app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
 app.use('/api/home/', homeRouter);
 app.use('/api/missions', missionsRouter);
 app.use('/api/profile', profileRouter);
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
 app.get('*', (req, res) => {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
