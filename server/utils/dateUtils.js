78f835fe6a402d7c93f09ea6bb19cbe0ab86fb6e
Hien Doan <dkhien.work@gmail.com>
2024-02-12 00:37:01 +0700

    fix: wrong first Monday of month calculation

--- server/utils/dateUtils.js
+++ server/utils/dateUtils.js
@@ -1,21 +1,19 @@
-function getFirstMondayOfMonth() {
-    const day = new Date();
-    day.setDate(1);
-    while (day.getDay() !== 1) {
-      day.setDate(day.getDate() + 1);
-    }
-    return day;
-  }
-  
-function getFirstMondayOfNextMonth() {
-    const day = new Date();
-    const month = day.getMonth() + 1;
-    day.setDate(1);
-    day.setMonth(month);
-    while (day.getDay() !== 1) {
-      day.setDate(day.getDate() + 1);
-    }
-    return day;
+const logger = require('../logger');
+
+function getFirstMondayOfMonth(date = new Date()) {
+  // Get the nearest previous Monday
+  const nearestMonday = new Date(date.toUTCString());
+  nearestMonday.setDate(nearestMonday.getDate() - nearestMonday.getDay() + (nearestMonday.getDay() === 0 ? -6 : 1));
+
+  // Get a day in week 1 of the month (by setting date to 7th)
+  const dayInWeek1 = new Date(Date.UTC(nearestMonday.getFullYear(), nearestMonday.getMonth(), 7));
+
+  // Get the nearest previous Monday of the day in week 1
+  const result = new Date(dayInWeek1);
+  result.setDate(dayInWeek1.getDate() - dayInWeek1.getDay() + (dayInWeek1.getDay() === 0 ? -6 : 1));
+  logger.debug(`First Monday of month is ${result.toDateString()}`);
+
+  return result;
 }
 
-module.exports = {getFirstMondayOfMonth, getFirstMondayOfNextMonth}
+module.exports = { getFirstMondayOfMonth }
