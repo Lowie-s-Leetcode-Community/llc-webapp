0e08e1c2b1aac694add892e80316e41667cfeaf3
Hien Doan <dkhien.work@gmail.com>
2024-02-12 10:56:18 +0700

    refactor: use modulo instead of ternary operator for fdom calculation

--- server/utils/dateUtils.js
+++ server/utils/dateUtils.js
@@ -12,14 +12,14 @@ const logger = require('../logger');
 function getFirstDayOfMonth(date = new Date()) {
   // Get the nearest previous Monday
   const nearestMonday = new Date(date.toUTCString());
-  nearestMonday.setDate(nearestMonday.getDate() - nearestMonday.getDay() + (nearestMonday.getDay() === 0 ? -6 : 1));
+  nearestMonday.setDate(nearestMonday.getDate() - (nearestMonday.getDay() + 6) % 7);
 
   // Get a day in week 1 of the month (by setting date to 7th)
   const dayInWeek1 = new Date(Date.UTC(nearestMonday.getFullYear(), nearestMonday.getMonth(), 7));
 
   // Get the nearest previous Monday of the day in week 1
   const result = new Date(dayInWeek1);
-  result.setDate(dayInWeek1.getDate() - dayInWeek1.getDay() + (dayInWeek1.getDay() === 0 ? -6 : 1));
+  result.setDate(dayInWeek1.getDate() - (dayInWeek1.getDay() + 6) % 7);
   logger.debug(`First day of month for ${date.toDateString()} is ${result.toDateString()}`);
 
   return result;
