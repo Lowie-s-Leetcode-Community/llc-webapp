feb8282847c4afb9fc5a036f588468e35fb00c43
lowie1803 <dung.totuan01@gmail.com>
2025-01-01 23:03:04 +0700

    Move backend to a separate repo

--- server/prisma/migrations/20240403074410_add_account/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Account" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "role" TEXT NOT NULL,
-
-    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
-);
